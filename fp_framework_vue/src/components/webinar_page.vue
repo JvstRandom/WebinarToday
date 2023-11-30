db8dafd3beed9fa970b687273af1da01276718ad
BintangNugraha0 <bintang.git@gmail.com>
2023-11-30 23:25:17 +0700

    cuma template

---
+++ fp_framework_vue/src/components/webinar_page.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="container">
+    <!-- ***** Banner Start ***** -->
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="main-profile">
+          <div class="row">
+            <div class="col-lg-4">
+              <img src="@/assets/img/portfolio/app1.jpg" alt="" style="border-radius: 23px;">
+            </div>
+            <div class="col-lg-4 align-self-center">
+              <div class="main-info header-text">
+                <span>Gratis</span>
+                <h4>Alan Smithee</h4>
+                <p>You Haven't Gone Live yet. Go Live By Touching The Button Below.</p>
+                <div class="main-border-button">
+                  <router-link to="#">Start Live Stream</router-link>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 align-self-center">
+              <ul>
+                <li>Games Downloaded <span>3</span></li>
+                <li>Friends Online <span>16</span></li>
+                <li>Live Streams <span>None</span></li>
+                <li>Clips <span>29</span></li>
+              </ul>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="clips">
+                <div class="row">
+                  <div class="col-lg-12">
+                    <div class="heading-section">
+                      <h4><em>Your Most Popular</em> Clips</h4>
+                    </div>
+                  </div>
+                  <!-- ... (lanjutkan dengan item klip) ... -->
+                </div>
+                <div class="col-lg-12">
+                  <div class="main-button">
+                    <router-link to="#">Load More Clips</router-link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- ***** Banner End ***** -->
+
+    <!-- ***** Gaming Library Start ***** -->
+    <div class="gaming-library profile-library">
+      <div class="col-lg-12">
+        <div class="heading-section">
+          <h4><em>Your Gaming</em> Library</h4>
+        </div>
+
+        <!-- Game Item 1 -->
+        <div class="item">
+          <ul>
+            <li><img src="@/assets/img/portfolio/app1.jpg" alt="" class="templatemo-item"></li>
+            <li><h4>Dota 2</h4><span>Sandbox</span></li>
+            <!-- ... (lanjutkan dengan informasi game lainnya) ... -->
+          </ul>
+        </div>
+
+        <!-- Game Item 2 -->
+        <div class="item">
+          <ul>
+            <li><img src="@/assets/img/portfolio/app1.jpg" alt="" class="templatemo-item"></li>
+            <li><h4>Fortnite</h4><span>Sandbox</span></li>
+            <!-- ... (lanjutkan dengan informasi game lainnya) ... -->
+          </ul>
+        </div>
+
+        <!-- Game Item 3 -->
+        <div class="item last-item">
+          <ul>
+            <li><img src="@/assets/img/portfolio/app1.jpg" alt="" class="templatemo-item"></li>
+            <li><h4>CS-GO</h4><span>Sandbox</span></li>
+            <!-- ... (lanjutkan dengan informasi game lainnya) ... -->
+          </ul>
+        </div>
+
+      </div>
+    </div>
+    <!-- ***** Gaming Library End ***** -->
+
+    <!-- Contact Section -->
+    <section id="contact">
+      <div class="container-fluid">
+        <!-- ... (bagian kontak) ... -->
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  // Komponen Vue.js
+};
+</script>
+
+<style scoped>
+  @import "@/assets/css/webpage.css";
+</style>
--- fp_framework_vue/src/router/index.js
+++ fp_framework_vue/src/router/index.js
@@ -1,23 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '@/components/HomePage.vue';
+import search_page from '@/components/search_page.vue';
+import P_Profile from '@/components/P_Profile.vue';
+import U_Profile from '@/components/P_Profile.vue';
+import webinar_page from '@/components/webinar_page.vue';
+import login from '@/components/login.vue';
+import Signup from '@/components/Signup.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/profile penyelenggara',
+    component: P_Profile,
+  },
+  {
+    path: '/profile user',
+    component: U_Profile,
+  },
+  {
+    path: '/search',
+    component: search_page,
+  },
+  {
+    path: '/page',
+    component: webinar_page,
+  },
+  {
+    path: '/login',
+    component: login,
+  },
+  {
+    path: '/sign up',
+    component: Signup,
+  },
+  // Tambahkan rute lain jika diperlukan
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  history: createWebHistory(),
+  routes
+});
 
-export default router
+export default router;
